a60359bec24fba522447e8695499d049af2e0f66
Nick Booher <njbooher@gmail.com>
2012-03-02 15:23:35 -0600

    Updated tree_score_test to reflect current base_task

--- util/tree_score_test.py
+++ util/tree_score_test.py
@@ -1,5 +1,9 @@
 import pickle
 
+#from os.path import abspath, dirname
+#import sys
+#sys.path.append(dirname(dirname(abspath(__file__))))
+
 from genome_scoring import SCORING_MATRIX, GENOME_FILE, TREE_FILE, BOUNDARY_FILE
 from sstree import PySSTree, ScoreTalentTask
 from blist import sortedlist
@@ -24,37 +28,12 @@ sTree = PySSTree(text, "load", TREE_FILE % ORGANISM)
 with open(BOUNDARY_FILE % ORGANISM, "r") as f:
     geneBoundaries = sortedlist(pickle.load(f), key=lambda x: x['pos'])
     
-subMatrix = {
-    "NG": ["T"],
-    "NN": ["A", "G"],
-    "HD": ["C"],
-    "NI": ["A"],
-    "NS": ["A"]
-}
-
-
-baseMap = {
-    'A': 0,
-    'C': 1,
-    'G': 2,
-    'T': 3
-}
-
-
-revBaseMap = {
-    'A': 3,
-    'C': 2,
-    'G': 1,
-    'T': 0
-}
-
-
 randMap = ["NG", "NN", "HD", "NI", "NS"]
 
 #querySequence = [randMap[random.randrange(0, 5, 1)] for i in range(1,22)]
 querySequence = "NN NG NN NG NG HD HD NG HD NI NN HD NG NN NG NN NG".split()
 
 def doIt():
-        ScoreTalentTask(string.join(querySequence, " "), "droso_standalone_seq_" + str(int(time.time())) + ".txt", True, geneBoundaries, scoringMatrix, sTree)
+        ScoreTalentTask(string.join(querySequence, " "), "droso_standalone_seq_" + str(int(time.time())), True, geneBoundaries, scoringMatrix, sTree)
         
 print timeit.Timer(doIt).timeit(1)
