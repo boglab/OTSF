5c9b3fdc8d7d9e4b0fb638097811c70fdef67620
Nick Booher <njbooher@gmail.com>
2012-03-09 23:20:01 -0600

    Clean up base_task and load scoring_matrix in Fortran order

--- genome_scoring/base_task.py
+++ genome_scoring/base_task.py
@@ -4,6 +4,7 @@ from celery.signals import worker_init
 
 import pickle
 import re
+import numpy as np
 
 from sstree import ScoreTalentTask
 from genome_scoring import SCORING_MATRIX, GENOME_FILE, TREE_FILE, BOUNDARY_FILE, DRUPAL_CALLBACK_URL
@@ -12,33 +13,10 @@ import urllib
 
 _workerState = {}
 
-_workerState["subMatrix"] = {
-    "NG": ["T"],
-    "NN": ["A", "G"],
-    "HD": ["C"],
-    "NI": ["A"],
-    "NS": ["A"]
-};
-
-_workerState["baseMap"] = {
-    'A': 0,
-    'C': 1,
-    'G': 2,
-    'T': 3
-};
-
-_workerState["revBaseMap"] = {
-    'A': 3,
-    'C': 2,
-    'G': 1,
-    'T': 0
-};
-
-_workerState["scoringMatrix"] = pickle.load(open(SCORING_MATRIX, "r"))
-
+_workerState["scoringMatrix"] = np.asfortranarray(pickle.load(open(SCORING_MATRIX, "r")))
 
 @task
-def TalentTask(querySequence, outputFilepath, nodeID, revComp):#, subMatrix, baseMap, scoringMatrix, sTree):
+def TalentTask(querySequence, outputFilepath, nodeID, revComp):
     ScoreTalentTask(querySequence, outputFilepath, revComp, _workerState["geneBoundaries"], _workerState["scoringMatrix"], _workerState["sTree"])
     if nodeID != -1:
         urllib.urlopen(DRUPAL_CALLBACK_URL + str(nodeID) + "/0")
